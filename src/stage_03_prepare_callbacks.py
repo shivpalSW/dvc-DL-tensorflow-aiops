a72cd11d8e22a2de79bc0bfa813b31c17f735da4
shivpalSW <shivpal.wadkar2050@gmail.com>
2023-07-10 16:37:43 +0530

    stage 03 base file ready

--- src/stage_03_prepare_callbacks.py
+++ src/stage_03_prepare_callbacks.py
@@ -1,9 +1,8 @@
 from src.utils.all_utils import read_yaml, create_directory
-from src.utils.models import get_VGG_16_model,prepare_model
+from src.utils.callbacks import create_and_save_tensorboard_callback, create_and_save_checkpoint_callback
 import argparse
 import os
 import logging
-import io
 
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
 log_dir = "logs"
@@ -11,9 +10,28 @@ os.makedirs(log_dir, exist_ok=True)
 logging.basicConfig(filename=os.path.join(log_dir, 'running_logs.log'), level=logging.INFO, format=logging_str,
                     filemode="a")
 
-
 def prepare_callbacks(config_path, params_path):
-    pass
+    config = read_yaml(config_path)
+    params = read_yaml(params_path)
+
+    artifacts = config["artifacts"]
+    artifacts_dir = artifacts["ARTIFACTS_DIR"]
+
+    tensorboard_log_dir = os.path.join(artifacts_dir, 
+    artifacts["TENSORBOARD_ROOT_LOG_DIR"])
+
+    checkpoint_dir = os.path.join(artifacts_dir, artifacts["CHECKPOINT_DIR"])
+    
+    callbacks_dir = os.path.join(artifacts_dir, artifacts["CALLBACKS_DIR"])
+
+    create_directory([
+        tensorboard_log_dir,
+        checkpoint_dir,
+        callbacks_dir
+    ])
+
+    create_and_save_tensorboard_callback(callbacks_dir, tensorboard_log_dir)
+    create_and_save_checkpoint_callback(callbacks_dir, checkpoint_dir)
 
 
 if __name__ == '__main__':
@@ -25,9 +43,9 @@ if __name__ == '__main__':
     parsed_args = args.parse_args()
 
     try:
-        logging.info(">>>>> stage Three started")
+        logging.info(">>>>> stage three started")
         prepare_callbacks(config_path=parsed_args.config, params_path=parsed_args.params)
-        logging.info("stage three completed! Callbacks are prepared and saved as binary >>>>>\n")
+        logging.info("stage three completed! callbacks are prepared and saved as binary>>>>>\n")
     except Exception as e:
         logging.exception(e)
         raise e
